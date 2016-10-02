7fa2391df0f88ab4cebd787831e55185ea438bba
Ken Snyder <ken@ken.net>
2016-10-02 17:16:08 +0100

    cmposable helpers and route-action helper

--- index.js
+++ index.js
@@ -2,5 +2,13 @@
 'use strict';
 
 module.exports = {
-  name: 'dummy-app-demo'
+  name: 'dummy-app-demo',
+  normalizeEntityName: function () { },
+  included: function (appOrAddon) {
+    this._super.included(app);
+    var app = appOrAddon.app || appOrAddon;
+    var bower = app.bowerDirectory;
+    app.import(bower + '/showdown/dist/showdown.js');
+
+  }
 };
